35006a3ac6a818394c4e07540e8833e38296406f
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2025-06-03 14:49:44 -0300

    refactor: Adicionando autoplay na seção Depoimentos

--- src/pages/Testimonials/index.jsx
+++ src/pages/Testimonials/index.jsx
@@ -1,7 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from "swiper/modules";
+import "swiper/css/autoplay";
+import { Autoplay, Pagination } from "swiper/modules";
 
 import "../../styles/custom-swiper-bullet.css";
 
@@ -37,10 +38,15 @@ export default function Testimonials() {
           O que os clientes dizem.
         </h1>
         <Swiper
-          modules={[Pagination]}
+          modules={[Pagination, Autoplay]}
           spaceBetween={20}
           slidesPerView={1}
           pagination={{ clickable: true }}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          loop={true}
           className="w-full max-w-7xl mx-auto shadow-lg cursor-pointer"
         >
           {testimonials.map((testimonial, index) => (
